e3e45da61328a3b2c537684535e380f962ab7346
Ali <abdulrehman76261@gmail.com>
2022-05-08 08:14:06 -0700

    random color generator

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const getColor = () => {
+    
+    const randomNumber = Math.floor(Math.random() * 16777215);
+    const randomCode = "#" + randomNumber.toString(16);
+    document.body.style.backgroundColor = randomCode;
+    document.getElementById("color-code").innerHTML = randomCode
+    // console.log(randomNumber , randomCode)
+    navigator.clipboard.writeText(randomCode);
+
+}
+
+document.getElementById("btn").addEventListener(
+    "click",
+    getColor
+)
+
+getColor();
